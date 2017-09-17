a6c32930c198961013f8b7de5446c23c6f6169e6
Lowell Burkett <laburket@asu.edu>
2017-09-17 14:06:08 -0500

    Initial project commit

---
+++ app/src/main/java/com/vot/lowellburkett/vot/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vot.lowellburkett.vot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
